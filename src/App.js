4b764e81802e7f3befc1870b9f0701b01fa0521f
Henry1998N <henry.nur137@gmail.com>
2023-04-14 13:43:25 +0300

    added feras description

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
           {house._id} - {house.name}
         </div>
       ))}
+      <h1>my name is feras wane</h1>
+      <h2>very small eggs and large ass</h2>
+      <img src="https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.6435-9/50616258_1174612056045609_928893175769071616_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=nnG5L_wHb10AX-x_-pD&_nc_ht=scontent.ftlv6-1.fna&oh=00_AfAMMPTk5mIpxvlacWVEYq7BHKkC87dXBN1VFXoVR8XRmg&oe=64609795" />
     </div>
   );
 }
